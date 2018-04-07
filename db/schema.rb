23af903d96636162613cfaac07f6c13091e0b263
Aljaz Kern <aljaz1.kern@gmail.com>
2018-04-07 09:10:52 +0000

    add admin user delete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403085031) do
+ActiveRecord::Schema.define(version: 20180407082304) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180403085031) do
     t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin",           default: false
   end
 
 end
