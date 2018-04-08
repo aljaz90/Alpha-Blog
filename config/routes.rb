134606a2d3d6acc77e5e96fda613ac70594dece4
Aljaz Kern <aljaz1.kern@gmail.com>
2018-04-08 08:44:52 +0000

    add categories index

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,15 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'signup', to: 'users#new'
-  resources :users, exept: [:new]
+  resources :users, except: [:new]
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'beta', to: 'sessions#beta'
+  
+  resources :categories, except: [:destroy]
   # You can have the root of your site routed with "root"
   #   root 'welcome#index'
 
